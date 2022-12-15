83f31930b9ab3da80aea90ed7332062cdd8d9a3f
moises <00400815@uca.edu.sv>
2022-12-15 15:08:25 -0600

    ejercicio 2

---
+++ E02/untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 2;
+
+        if (num1 == num2)
+            System.out.println("num1 y num2 son iguales");
+
+        if (num1>num2)
+            System.out.println("num1 es mayor");
+
+        if (num2>num1)
+            System.out.println("num2 es mayor");
+    }
+}
