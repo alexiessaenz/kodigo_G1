e9c0cbe5a84c9ebe32488cf7be2f900b3ad1ea61
moises <00400815@uca.edu.sv>
2022-12-16 16:30:01 -0600

    feat: exercise 12 fineshed

---
+++ E12/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++)
+            System.out.println("99\tx\t" + i + "\t=\t" + (99 * i));
+    }
+}
