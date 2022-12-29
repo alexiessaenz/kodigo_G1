48985a53eb9d0b6f1b9f02ba9b64df7eaebf63fe
moises <00400815@uca.edu.sv>
2022-12-28 23:50:24 -0600

    fgh

--- E18/demo/src/main/java/com/example/App.java
+++ E18/demo/src/main/java/com/example/App.java
@@ -12,6 +12,7 @@ public class App
     public static void main( String[] args ){
         Scanner scan =  new Scanner(System.in);
         int num1, num2, num3;
+        
         System.out.println( "Ingrese dos numeros limites: " );
         num1 = scan.nextInt();
         num2 = scan.nextInt();     // 0.0 - 1.0 ( 10 - 1 + 1 ) + 1 
