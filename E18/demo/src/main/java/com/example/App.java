c7bd8795b20492b032a7e5ae2fd432279d4a1439
moises <00400815@uca.edu.sv>
2022-12-28 23:52:12 -0600

    ghf

--- E18/demo/src/main/java/com/example/App.java
+++ E18/demo/src/main/java/com/example/App.java
@@ -12,11 +12,12 @@ public class App
     public static void main( String[] args ){
         Scanner scan =  new Scanner(System.in);
         int num1, num2, num3;
-        
+
         System.out.println( "Ingrese dos numeros limites: " );
         num1 = scan.nextInt();
         num2 = scan.nextInt();     // 0.0 - 1.0 ( 10 - 1 + 1 ) + 1 
         num3 = (int) (Math.random() * (num2 - num1 + 1) + num1);
+        
         System.out.println( "El numero aleatorio es: " + num3);
         scan.close();
     }
