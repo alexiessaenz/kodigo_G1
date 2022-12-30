7d1194be415b1e74deea85f1b35e30b804d50c2f
moises <00400815@uca.edu.sv>
2022-12-29 19:45:41 -0600

    feat: exercise 19 finished

--- E19/src/main/java/org/example/Main.java
+++ E19/src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String frase;
+        int opcion;
+        System.out.println("Ingresa una frase: ");
+        frase = scan.nextLine();
+        System.out.println("Convertir:\n1. Mayúsculas\n2. Minúsculas");
+        opcion = scan.nextInt();
+
+        switch (opcion) {
+            case 1 -> {
+                System.out.println(frase.toUpperCase());
+            }
+            case 2 -> System.out.println(frase.toLowerCase());
+            default -> System.out.println("Opción no válida");
+        }
+        scan.close();
     }
 }
